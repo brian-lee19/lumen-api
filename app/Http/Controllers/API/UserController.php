3aaac793160cb7eca5468d785f86d37561368e79
Lee Vern Hui <vernhuilee@gmail.com>
2019-07-31 12:45:31 +0800

    Add register endpoint

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -36,4 +36,31 @@ class UserController extends Controller
             return response($response, $this->unprocessableEntity);
         }
     }
+    /** 
+     * Register api 
+     * 
+     * @return \Illuminate\Http\Response 
+     */ 
+    public function register(Request $request) 
+    { 
+        $validator = Validator::make($request->all(), [
+            'fullname' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response(['errors'=>$validator->errors()->all()], $this->unprocessableEntity);
+        }
+
+        $request['password']=Hash::make($request['password']);
+        $user = User::create($request->toArray());
+
+        $token = $user->createToken('Laravel Password Grant Client')->accessToken;
+        $response = ['token' => $token];
+
+        return response($response, $this->successStatus);
+
+    }
+    
 }
